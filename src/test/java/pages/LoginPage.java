11675172b5f50e71dc42022c9882fdc0d390b49f
abinash <dj.avinash55@gmail.com>
2022-06-14 18:37:21 +0530

    jhhj

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,11 +1,30 @@
 package pages;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 	
 	@FindBy(name="identifier") public WebElement username;
+	
+	@FindBy(xpath=("//span[text()='Next']/parent::button")) public WebElement homeNext;
+	
+	public LoginPage(RemoteWebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void fillUsername(String x)
+	{
+		username.sendKeys(x);
+	}
+	
+	public void clickNext()
+	{
+		homeNext.click();
+	}
 	
 	
 
