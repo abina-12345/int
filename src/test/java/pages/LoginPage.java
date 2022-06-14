d93635b13d902686da62ce38914b1acf22302235
abinash <dj.avinash55@gmail.com>
2022-06-14 17:06:14 +0530

    asdf

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,12 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+	
+	@FindBy(name="identifier") public WebElement username;
+	
+	
 
 }
